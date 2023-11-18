79214ca183a12d41e20b6084e90eba9cc4e8f43a
Nicholas <nwoo5@uwo.ca>
2023-11-18 16:23:17 -0500

    Put API key in empty quotes in ai.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import ai
-openai_api_key = "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"
-response = ai.get_response("Nicholas Woo", "I am feeling chest pains, as if my chest is tearing")
+from openai import OpenAI
 
+import ai
+response = ai.get_response("Nicholas Woo", "I am feeling chest pains, as if my chest is tearing")
+client = OpenAI()
 print(response)
