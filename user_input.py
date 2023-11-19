a6ac874008ab3c9e47c368da391282e2162e8454
Nicholas <nwoo5@uwo.ca>
2023-11-18 23:15:30 -0500

    Added prompt to give possible conditions.

--- user_input.py
+++ user_input.py
@@ -5,6 +5,7 @@ def get_name(sentence):
     name = ai.get_patient_name(sentence)
     return name
 
+
 def get_symptoms(sentence):
     str = ai.get_patient_symptoms(sentence)
     symptoms = str.split(",")
@@ -12,6 +13,7 @@ def get_symptoms(sentence):
     for symptom in symptoms:
         symptom = symptom.rstrip()
     return symptoms
+
 
 def translate_symptoms(symptoms):
     str = ""
@@ -24,3 +26,17 @@ def translate_symptoms(symptoms):
     for medical_term in medical_terms:
         medical_term = medical_term.rstrip()
     return medical_terms
+
+
+def get_possible_conditions(layman_terms, medical_terms):
+    str = "Layman Terms: "
+    for layman in layman_terms:
+        str += layman + ", "
+
+    str += " Medical_Terms: "
+    for medical in medical_terms:
+        str += medical + ", "
+
+    str = ai.get_possible_conditions(str)
+
+    return str
